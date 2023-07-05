71a226adec832bcab9fcbed4c4cc5672de577247
Akarsh Hegde <akarshhegde1001@gmail.com>
2023-07-05 13:50:04 +0530

    Create 5_Simple_patterns.java

---
+++ 5_Simple_patterns.java
@@ -0,0 +1,25 @@
+//1]
+public class july01{
+    public static void main(String[] args) {
+        for(int line=1; line<=4; line++){
+            for(int star=1; star<=line; star++){
+                System.out.print("*");
+            }
+            
+        }
+    }
+}
+
+public class july01{
+    public static void main(String[] args) {
+        int n=4;
+        for(int line=1; line<=n; line++){
+            for(int star=1; star<=n-line+1; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//2]
