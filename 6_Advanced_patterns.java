bb973c92d8ab873aea55cc96892ca658830c78f5
Akarsh Hegde <akarshhegde1001@gmail.com>
2023-07-09 09:37:22 +0530

    6_Advanced_patterns.java

--- 6_Advanced_patterns.java
+++ 6_Advanced_patterns.java
@@ -37,3 +37,41 @@ public class july06{
         zero_one_triangle(5);
     }
 }
+
+//3]
+public class july06{
+    public static void butterfly(int n) {
+        //1st Half
+        for(int i=1; i<=n; i++){
+            //stars - i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            //spaces - 2*(n-i)
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            //star - i
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // 2nd Half
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=1; j<=2*(n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+         }
+    }
+    public static void main(String[] args) {
+        butterfly(5);
+    }
+}
