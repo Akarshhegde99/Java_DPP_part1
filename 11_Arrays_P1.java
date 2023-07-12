0c1b69a6916b1704c9d72d6ec09861d9cb6085fb
Akarsh Hegde <akarshhegde1001@gmail.com>
2023-07-12 23:27:50 +0530

    11_Arrays_P1.java

--- 11_Arrays_P1.java
+++ 11_Arrays_P1.java
@@ -45,3 +45,30 @@ public class july12{
         }
     }
 
+//3]
+
+import java.util.*;
+
+public class july11{
+    public static int getLargest(int numbers[]) {
+        int largest =Integer.MIN_VALUE;  //-Infinity
+        int smallest =Integer.MAX_VALUE; 
+
+        for(int i=0; i<numbers.length; i++){
+            if(largest<numbers[i]){
+                largest = numbers[i];
+            }
+            if(smallest>numbers[i]){
+                smallest = numbers[i];
+            }
+        }
+        System.out.println("Smallest value is : "+ smallest);
+
+        return largest;
+    }
+    public static void main(String[] args) {
+        int numbers[] = {1,2,6,3,5};
+        System.out.println("Largest value :"+getLargest(numbers));
+    }
+}
+
