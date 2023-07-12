9a42748543e8104daf0d77cc191d230d1d83dc8e
Akarsh Hegde <akarshhegde1001@gmail.com>
2023-07-12 23:27:06 +0530

    11_Arrays_P1.java

---
+++ 11_Arrays_P1.java
@@ -0,0 +1,47 @@
+//1]
+
+import java.util.*;
+public class july12 {
+    public static void main(String[] args) {
+        int marks[]= new int[100];
+
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Length of array : "+ marks.length);
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        System.out.println("Physics : "+ marks[0]);
+        System.out.println("Chemistry : "+ marks[1]);
+        System.out.println("Maths : "+ marks[2]);
+
+        marks[1]=marks[1]+1;
+        System.out.println("Chemistry (New marks) : "+ marks[1]);
+    }
+}
+
+//2]
+import java.util.*;
+public class july12{
+    public static int linearSearch(int numbers[], int key) {
+        
+        for(int i=0; i<numbers.length; i++){
+            if(numbers[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int numbers[] ={2,4,6,8,10,12,14,16};
+        int key =18;
+
+        //H.W  String menu[] ={"dosa","Chole bature", "Samosa"}
+        int index=linearSearch(numbers, key);
+        if(index==-1){
+            System.out.println("NOT FOUND");
+        }else{
+            System.out.println("Key is at index : "+index);
+        }
+    }
+
