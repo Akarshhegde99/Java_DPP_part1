ddcc6f088a45644eb894b6b38c4fb7eab0359e03
Akarsh Hegde <akarshhegde1001@gmail.com>
2023-07-14 18:38:20 +0530

    14_Arrays_P3.java

--- 14_Arrays_P3.java
+++ 14_Arrays_P3.java
@@ -34,3 +34,24 @@ public class july15{
         System.out.println("Minimum value for the above array = " + min);
     }
 }
+
+//2
+
+public class july15{
+  public static boolean contains(int[] arr, int item) {
+      for (int n : arr) {
+         if (item == n) {
+            return true;
+         }
+      }
+      return false;
+   }
+   public static void main(String[] args) {
+          int[] my_array1 = {
+            1789, 2035, 1899, 1456, 2013, 
+            1458, 2458, 1254, 1472, 2365, 
+            1456, 2265, 1457, 2456};
+      System.out.println("\n"+contains(my_array1, 2013));
+      System.out.println("\n"+contains(my_array1, 2015)+"\n");
+   }
+}
